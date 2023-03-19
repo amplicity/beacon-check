3b1746973706a2720d770bb22592133361550612
James Diecidue <jamesdiecidue@gmail.com>
2023-03-18 22:05:21 -0400

    tested

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-index.js
+require('dotenv').config();
+const axios = require('axios');
+
+exports.handler = async () => {
+	console.log('checking your node...', process.env.PUBLIC_KEY);
+	axios.get(process.env.REQUEST_URL + process.env.PUBLIC_KEY + '/balancehistory',{}).then((res) => {
+		const balanceHistory = res.data.data;
+		let balanceWentDown = false;
+		for (let i = 0; i < 10; i++) {
+			// console.log('balanceHistory for index ' + i, balanceHistory[i]);
+			let prev = balanceHistory[i];
+			if (prev.balance < balanceHistory[i+1].balance ){
+				balanceWentDown = true;
+				console.log('prev in here', prev)
+				console.log('current in here', balanceHistory[i+1])
+				break;
+			}
+		}
+
+		if (balanceWentDown){
+			axios.post(process.env.SLACK_WEBHOOK_URL, {text: '😱 Node balance has decreased. Check ' + process.env.BEACONCHAIN_URL + process.env.PUBLIC_KEY})
+		} else {
+			axios.post(process.env.SLACK_WEBHOOK_URL, {text: '👍 Node balance is fine. Check ' + process.env.BEACONCHAIN_URL + process.env.PUBLIC_KEY})
+		}
+
+	}).catch((err) => {
+		console.error(err);
+	});
+	
+}
