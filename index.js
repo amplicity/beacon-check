65a9c66400e2cf5a2882c4599e05742b9658e4c1
James Diecidue <jamesdiecidue@gmail.com>
2023-03-20 10:12:37 -0400

    fix

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const axios = require('axios');
 
 exports.handler = async () => {
 	console.log('checking your node...', process.env.PUBLIC_KEY);
-	axios.get(process.env.REQUEST_URL + process.env.PUBLIC_KEY,{}).then((res) => {
-		const response = res.data.data[0];
-		if (response.status == "active_offline"){
-			console.log('Your node is offline!');
-			axios.post(process.env.SLACK_WEBHOOK_URL, {text: "📣 Your node is offline!"});
-		}
-		else {
-			console.log("👍 Your node is online.");
-		}
-	}).catch((err) => {
-		console.error(err);
-	});
-	
+	const response = await axios.get(process.env.REQUEST_URL + process.env.PUBLIC_KEY,{});
+	const data = response.data.data[0];
+	if (data.status !== "active_online"){
+		await axios.post(process.env.SLACK_WEBHOOK_URL, {text: "📣 Your node is offline!"});
+		console.log('Your node is offline!');
+	}
+	else {
+		console.log("👍 Your node is online.");
+	}
 }
+
